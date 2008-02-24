a1241bedd97a75db390d73fd16a9ba7b27be4cc1
sun <sun@unleashedmind.com>
2008-02-24 19:45:40 +0000

    Syncing DRUPAL-5--2 with latest code in DRUPAL-6--1.

--- scripts/coder_format/coder_format.cmd
+++ scripts/coder_format/coder_format.cmd
@@ -3,50 +3,25 @@ rem $Id$
 
 rem Define Coder Format shell invocation script path.
 set coderFormatPath=c:\program files\coder_format
-rem Define location of Drupal's file.inc.
-set fileInc=c:\program files\coder_format\file.inc
 
 
 :: ----- You should not need to edit anything below. ----- ::
 
+if "%~1"=="" goto error
 set oldpwd=%CD%
 cd /d "%coderFormatPath%"
 
-rem Simple file extension check, won't work with directories containing a dot.
-if '%1'=='--undo' goto undo-directory
-if '%~x1'=='' goto directory
-goto file
+rem Check if at least one argument follows a --undo parameter.
+if "%~1"=="--undo" if "%~2"=="" goto error
 
-:undo-directory
-rem Undo source code formattings performed by --batch-replace
-rem Process directory.
-if "%~2"=="" goto :EOF
-start "coder_format" /D "%coderFormatPath%" /B /WAIT php coder_format.php --undo "%~2" --file-inc "%fileInc%"
-goto processed
+start "coder_format" /D "%coderFormatPath%" /B /WAIT php coder_format.php %*
+goto done
 
-:directory
-rem Recursively format all source code files in a directory.
-rem Process directory.
-if "%~1"=="" goto :EOF
-start "coder_format" /D "%coderFormatPath%" /B /WAIT php coder_format.php --batch-replace "%~1" --file-inc "%fileInc%"
-goto processed
+:error
+echo ERROR: Wrong arguments supplied. Please see usage in coder_format.php.
+goto :EOF
 
-:file
-rem Format a single source code file.
-rem Define source and target files by command line arguments.
-set sourcefile=%~1
-if not '%~2'=='' (
-	set targetfile=%~2
-) else (
-	set targetfile=%~1
-)
-rem Process file.
-if "%sourcefile%"=="" goto :EOF
-if "%targetfile%"=="" goto :EOF
-start "coder_format" /D "%coderFormatPath%" /B /WAIT php coder_format.php "%sourcefile%" "%targetfile%" --file-inc "%fileInc%"
-
-
-:processed
+:done
 rem Jump back to original working directory.
 cd /d "%oldpwd%"
 
