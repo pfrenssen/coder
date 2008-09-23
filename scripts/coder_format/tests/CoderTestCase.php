64ecee4eb81eea01b2fa1caef9eb13be46fa1eb1
Doug Green <douggreen@douggreenconsulting.com>
2008-09-23 14:35:59 +0000

    upgrade simpletests to the 6.2 framework

--- scripts/coder_format/tests/CoderTestCase.php
+++ scripts/coder_format/tests/CoderTestCase.php
@@ -3,7 +3,7 @@
 
 require_once drupal_get_path('module', 'coder') .'/scripts/coder_format/coder_format.inc';
 
-class CoderTestCase extends DrupalTestCase {
+class CoderTestCase extends DrupalWebTestCase {
   function assertFormat($input, $expect) {
     $result = coder_format_string_all($input);
     $this->assertIdentical($result, $input);
