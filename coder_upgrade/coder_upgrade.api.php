410133f72c50703b5624e27d38a4a0fc6c16a3da
Jim Berry <solotandem@240748.no-reply.drupal.org>
2009-08-01 12:55:09 +0000

    Revise user interface using new D7 form elements; implement hook_upgrades; revise help text.

---
+++ coder_upgrade/coder_upgrade.api.php
@@ -0,0 +1,16 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * The api documentation for the coder_upgrade module.
+ *
+ * Copyright 2008-9 by Jim Berry ("solotandem", http://drupal.org/user/240748)
+ */
+
+/**
+ * Documentation of hook_upgrades.
+ */
+function hook_upgrades()  {
+  //
+}
