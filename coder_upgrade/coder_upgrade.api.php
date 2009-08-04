e157919a6b490b43203986b5580ed1aa15241992
Jim Berry <solotandem@240748.no-reply.drupal.org>
2009-08-04 16:10:47 +0000

    Rename hook_perm and the permission; Revise API documentation.

--- coder_upgrade/coder_upgrade.api.php
+++ coder_upgrade/coder_upgrade.api.php
@@ -3,65 +3,110 @@
 
 /**
  * @file
- * The api documentation for the coder_upgrade module.
- *
- * Instructions:
- * - Add the contents of this file to a file in your module.
- * - Replace "your_module_name" below with your actual module name.
- * - Replace other "your_" occurrences with actual values.
- * - Complete the conversions_list() routine.
- *
+ * Hooks provided by the Coder Upgrade module.
+ * 
  * Copyright 2008-9 by Jim Berry ("solotandem", http://drupal.org/user/240748)
  */
 
 /**
- * Implement hook_upgrades().
- *
- * NOTE: Change this function name to your_module_name_upgrades.
- * 
- * @todo Add Doxygen style comments with API documentation.
- *
- * @param boolean $include_routines
- *   Whether to include the list of routines (only needed on submit).
+ * @addtogroup hooks
+ * @{
  */
-function hook_upgrades($include_routines)  {
-  global $_coder_upgrades;
-
-  // If your module has multiple files defining upgrade routines, then refer
-  // to coder_upgrade.main.inc for an example with a loop.
-  if (!isset($_coder_upgrades)) {
-    $_coder_upgrades = array();
-    $path = drupal_get_path('module', 'your_module_name') . '/your_subdirectory/your_filename';
-    require_once DRUPAL_ROOT . '/' . $path;
-    $function = str_replace('.', '_', $file->name) . '_upgrades';
-    if (drupal_function_exists($function)) {
-      if ($upgrade = call_user_func($function, $include_routines)) {
-        $_coder_upgrades = array_merge($_coder_upgrades, $upgrade);
-      }
-    }
-  }
-  return $_coder_upgrades;
-}
 
 /**
- * Callback from hook_upgrades().
+ * Declare upgrade sets for an API (or set of APIs).
  *
+ * This hook allows contributed modules to declare upgrade sets for an API
+ * supplied by their module, another contributed module, or a core module. Each
+ * upgrade set may optionally include a list of conversion routines.
+ * 
+ * For example, if your module is called "your_module_name," then declare an
+ * upgrade set as:
+ * @code
+ *   function your_module_name_upgrades($include_routines) {
+ *     $routines = $include_routines ? your_module_name_conversions_list() : array();
+ *     $upgrade = array(
+ *       'title' => t('Your module API changes from 6.x to 7.x'),
+ *       'link' => 'http://...',
+ *       'routines' => $routines,
+ *     );
+ *     return array('your_module_name' => $upgrade);
+ *   }
+ * @endcode
+ * 
+ * The naming convention for a conversion routine is:
+ * @code
+ *   your_module_name_convert_your_routine_name();
+ * @endcode
+ * 
+ * The array returned by your_module_name_conversions_list() should be a list
+ * of the suffixes of your conversion routines. The suffix is the portion of
+ * the routine name following "_convert_" above.
+ * @code
+ *   function your_module_name_conversions_list() {
+ *     return array(
+ *       'your_routine_name',
+ *     );
+ *   }
+ * @endcode
+ *
+ * The $file parameter passed to your conversion routine is a reference to the
+ * text of the code file to be upgraded. Be sure to include an ampersand before
+ * $file so that your changes will be incorporated into the output code file.
+ * @code
+ *   function your_module_name_convert_your_routine_name(&$file) {
+ *     // Do something to $file.
+ *   }
+ * @endcode
+ * 
+ * To use the sample functions included in this api file:
+ * - Copy and paste the sample functions to a file in your module.
+ * - Replace "your_module_name" with your actual module name.
+ * - Complete the conversions_list() routine.
+ * - Duplicate the sample conversion routine for each entry in your list,
+ *   replacing "your_routine_name" with the appropriate value and changing the
+ *   comment block to describe the upgrade applied by the routine.
+ * 
  * @param boolean $include_routines
- *   Whether to include the list of routines (only needed on submit).
+ *   Indicates whether to include the list of conversion routines. This list
+ *   is only needed when the conversions are to be applied (typically on form
+ *   submission). This parameter equals FALSE when this hook is invoked by
+ *   Coder Upgrade while building the form, and TRUE when applying the
+ *   conversions.
  */
-function your_module_name_list_upgrades($include_routines) {
+function hook_upgrades($include_routines)  {
   $routines = $include_routines ? your_module_name_conversions_list() : array();
   $upgrade = array(
     'title' => t('Your module API changes from 6.x to 7.x'),
     'link' => 'http://...',
     'routines' => $routines,
-    'severity' => 'critical',
   );
-  return array('coder_upgrade' => $upgrade);
+  return array('your_module_name' => $upgrade);
 }
 
 /**
- * Return a list of conversion routines.
+ * @} End of "addtogroup hooks".
+ */
+
+/**
+ * Sample functions.
+ */
+
+/**
+ * Implement hook_upgrades().
+ */
+function your_module_name_upgrades($include_routines)  {
+  $routines = $include_routines ? your_module_name_conversions_list() : array();
+  $upgrade = array(
+    'title' => t('Your module API changes from 6.x to 7.x'),
+    'link' => 'http://...',
+    'routines' => $routines,
+  );
+  return array('your_module_name' => $upgrade);
+}
+
+/**
+ * Return a list of conversion routine suffixes.
  *
  * The complete function name that Coder Upgrade searches for is:
  *   <your_module_name> . '_convert_' . <your_routine_name>
@@ -82,7 +127,7 @@ function your_module_name_conversions_list() {
  * Describe the upgrade applied by this routine.
  *
  * @param string $file
- *   The file to convert.
+ *   The text of the file to convert.
  */
 function your_module_name_convert_your_routine_name(&$file) {
   // Do something to $file.
