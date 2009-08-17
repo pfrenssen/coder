fa08093d3d883e30e303e8b888e40967eadebc6d
Jim Berry <solotandem@240748.no-reply.drupal.org>
2009-08-17 16:04:46 +0000

    Group conversion routines by processing order (beginning, middle, or end). Separate middle conversion routines into two groups (regex and parser) to utilize the PGP API. Add handler specification (regex or parser) for the middle changes applied at the file level. Revise API documentation.

--- coder_upgrade/coder_upgrade.api.php
+++ coder_upgrade/coder_upgrade.api.php
@@ -4,7 +4,7 @@
 /**
  * @file
  * Hooks provided by the Coder Upgrade module.
- * 
+ *
  * Copyright 2008-9 by Jim Berry ("solotandem", http://drupal.org/user/240748)
  */
 
@@ -19,7 +19,7 @@
  * This hook allows contributed modules to declare upgrade sets for an API
  * supplied by their module, another contributed module, or a core module. Each
  * upgrade set may optionally include a list of conversion routines.
- * 
+ *
  * For example, if your module is called "your_module_name," then declare an
  * upgrade set as:
  * @code
@@ -33,32 +33,67 @@
  *     return array('your_module_name' => $upgrade);
  *   }
  * @endcode
- * 
+ *
  * The naming convention for a conversion routine is:
  * @code
  *   your_module_name_convert_your_routine_name();
  * @endcode
- * 
+ *
  * The array returned by your_module_name_conversions_list() should be a list
  * of the suffixes of your conversion routines. The suffix is the portion of
  * the routine name following "_convert_" above.
+ *
+ * Group changes by processing order (beginning, middle, or end). The beginning
+ * and end conversion routines are applied only at the module or directory
+ * level and the routine will need to determine what to do and how (regex or
+ * parser) to do it.
+ *
+ * The middle changes are applied at the file level and the list entry must
+ * specify the handler (regex or parser). The regex changes are applied before
+ * the parser changes.
  * @code
  *   function your_module_name_conversions_list() {
- *     return array(
- *       'your_routine_name',
+ *     $list = array(
+ *       'beginning' => array(),
+ *       'middle' => array(),
+ *       'end' => array(),
  *     );
+ *
+ *     $list['middle'] = array(
+ *       array(
+ *         'name' => 'your_routine_name_regex',
+ *         'type' => 'regex',
+ *       ),
+ *       array(
+ *         'name' => 'your_routine_name_parser',
+ *         'type' => 'parser',
+ *       ),
+ *     );
+ *
+ *     return $list;
  *   }
  * @endcode
  *
- * The $file parameter passed to your conversion routine is a reference to the
- * text of the code file to be upgraded. Be sure to include an ampersand before
- * $file so that your changes will be incorporated into the output code file.
+ * The $file parameter passed to your regex conversion routine is a reference
+ * to the text of the code file to be upgraded. Be sure to include an ampersand
+ * before $file so that your changes will be incorporated into the output code
+ * file.
  * @code
- *   function your_module_name_convert_your_routine_name(&$file) {
+ *   function your_module_name_convert_your_routine_name_regex(&$file) {
  *     // Do something to $file.
  *   }
  * @endcode
- * 
+ *
+ * The $reader parameter passed to your parser conversion routine is a
+ * reference to the object containing the grammar statements of the file to
+ * convert. Be sure to include an ampersand before $reader so that your changes
+ * will be incorporated into the output code file.
+ * @code
+ *   function your_module_name_convert_your_routine_name_parser(&$reader) {
+ *     // Do something with $reader.
+ *   }
+ * @endcode
+ *
  * To use the sample functions included in this api file:
  * - Copy and paste the sample functions to a file in your module.
  * - Replace "your_module_name" with your actual module name.
@@ -66,7 +101,7 @@
  * - Duplicate the sample conversion routine for each entry in your list,
  *   replacing "your_routine_name" with the appropriate value and changing the
  *   comment block to describe the upgrade applied by the routine.
- * 
+ *
  * @param boolean $include_routines
  *   Indicates whether to include the list of conversion routines. This list
  *   is only needed when the conversions are to be applied (typically on form
@@ -118,9 +153,24 @@ function your_module_name_upgrades($include_routines)  {
  *   Array of conversion routine suffixes.
  */
 function your_module_name_conversions_list() {
-  return array(
-    'your_routine_name',
+  $list = array(
+    'beginning' => array(),
+    'middle' => array(),
+    'end' => array(),
   );
+
+  $list['middle'] = array(
+    array(
+      'name' => 'your_routine_name_regex',
+      'type' => 'regex',
+    ),
+    array(
+      'name' => 'your_routine_name_parser',
+      'type' => 'parser',
+    ),
+  );
+
+  return $list;
 }
 
 /**
@@ -129,6 +179,28 @@ function your_module_name_conversions_list() {
  * @param string $file
  *   The text of the file to convert.
  */
-function your_module_name_convert_your_routine_name(&$file) {
+function your_module_name_convert_your_routine_name_regex(&$file) {
   // Do something to $file.
+  $hook = 'your_changes'; // Used as the label in the log file.
+  $cur = $file;
+  $new = $cur;
+
+  $from = array();
+  $to = array();
+
+  $from[] = '/(your_module_name)/';
+  $to[] = "$1";
+
+  coder_upgrade_do_conversions($from, $to, $new);
+  coder_upgrade_save_changes($cur, $new, $file, $hook);
+}
+
+/**
+ * Describe the upgrade applied by this routine.
+ *
+ * @param PGPReader $reader
+ *   The object containing the grammar statements of the file to convert.
+ */
+function your_module_name_convert_your_routine_name_parser(&$reader) {
+  // Do something with $reader.
 }
