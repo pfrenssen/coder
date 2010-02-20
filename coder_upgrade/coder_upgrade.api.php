e33f55c74895fc2f9658b2b17aa781622bda9b7c
Jim Berry <solotandem@240748.no-reply.drupal.org>
2010-02-20 17:33:30 +0000

    coder_upgrade.api.inc: Update API documentation for new alter hooks; coder_upgrade.main.inc: Add hook_upgrade_hook_alter, use old directory name in hook_upgrade_end_alter if files are being replaced.

--- coder_upgrade/coder_upgrade.api.php
+++ coder_upgrade/coder_upgrade.api.php
@@ -14,6 +14,47 @@
  */
 
 /**
+ * Declares upgrade sets for an API (or set of APIs).
+ *
+ * This hook allows contributed modules to declare upgrade sets for an API
+ * supplied by their module, another contributed module, or a core module. To be
+ * able to apply the upgrade routines independently, the upgrade routines should
+ * be contained in separate files.
+ *
+ * For example, if your module is called 'your_module_name' and its upgrade
+ * routines are in 'your_module_name.upgrade' (the default file name), then
+ * declare an upgrade set as:
+ * @code
+ *   function your_module_name_upgrade_info() {
+ *     $upgrade = array(
+ *       'title' => t('Your module API changes from 6.x to 7.x'),
+ *       'link' => 'http://...',
+ *     );
+ *     return array('your_module_name' => $upgrade);
+ *   }
+ * @endcode
+ *
+ * @return
+ *   An associative array (keyed on the module name) with each element being an
+ *   associative array with the following elements:
+ *   - 'title': A description of the upgrade routines provided by the upgrade set.
+ *   - 'link': An optional link to an issue describing the upgrade routines.
+ *   - 'files': An optional array of file names containing the upgrade routines.
+ *     The name includes any relative path inside the module directory. Defaults
+ *     to 'your_module_name.upgrade'.
+ */
+function hook_upgrade_info() {
+  $upgrade = array(
+    'title' => t('Your module API changes from 6.x to 7.x'),
+    'link' => 'http://...',
+    'files' => array(
+      'upgrades/your_module_name.upgrade',
+    ),
+  );
+  return array('your_module_name' => $upgrade);
+}
+
+/**
  * Alters a function call using the grammar parser.
  *
  * This hook allows contributed modules to alter a function call object using
@@ -48,6 +89,44 @@ function hook_upgrade_call_FUNCTION_NAME_alter(&$item, &$reader) {
 }
 
 /**
+ * Alters function calls using the grammar parser.
+ *
+ * This hook allows contributed modules to alter any function call object using
+ * the grammar parser. The function call may be a stand-alone statement or part
+ * of an expression in another statement. For example:
+ * @code
+ *   foo($bar); // Stand-alone.
+ *   if (foo($bar)) { // Embedded.
+ *     // Do something.
+ *   }
+ * @endcode
+ *
+ * Coder Upgrade will call this alter hook for each function call in the file
+ * that was parsed. Refer to the grammar parser documentation for details of the
+ * function call object.
+ *
+ * @see PGPFunctionCall
+ *
+ * @param PGPFunctionCall $item
+ *   A function call object of the expression or statement.
+ * @param PGPReader $reader
+ *   The object containing the grammar statements of the file to convert.
+ * @param $name
+ *   The name of the function.
+ */
+function coder_upgrade_upgrade_call_alter(&$item, &$reader, $name) {
+  // Modify function call.
+  switch ($name) {
+    case 'foo':
+      $item->deleteParameter();
+      break;
+
+    default:
+      break;
+  }
+}
+
+/**
  * Alters a hook function using the grammar parser.
  *
  * This hook allows contributed modules to alter a function object using the
@@ -78,7 +157,7 @@ function hook_upgrade_call_FUNCTION_NAME_alter(&$item, &$reader) {
  * Refer to the grammar parser documentation for details of the function object
  * (i.e. PGPClass).
  *
- * @see hook_upgrades
+ * @see hook_upgrade_file_alter
  * @see PGPClass
  *
  * @param PGPNode $node
@@ -107,108 +186,230 @@ function hook_upgrade_hook_HOOK_NAME_alter(&$node, &$reader) {
 }
 
 /**
- * Declares upgrade sets for an API (or set of APIs).
+ * Alters hook functions using the grammar parser.
  *
- * This hook should only be used if the conversion routines are other than a
- * change to a function call or to a hook function. The Coder Upgrade module
- * already invokes an alter hook that allows any function call or hook function
- * to be changed.
- *
- * This hook allows contributed modules to declare upgrade sets for an API
- * supplied by their module, another contributed module, or a core module. Each
- * upgrade set may optionally include a list of conversion routines.
- *
- * For example, if your module is called "your_module_name," then declare an
- * upgrade set as:
+ * This hook allows contributed modules to alter any function object using the
+ * grammar parser. The function block may be inside an interface or class, or a
+ * stand-alone statement block. For example:
  * @code
- *   function your_module_name_upgrades($include_routines) {
- *     $routines = $include_routines ? your_module_name_conversions_list() : array();
- *     $upgrade = array(
- *       'title' => t('Your module API changes from 6.x to 7.x'),
- *       'link' => 'http://...',
- *       'routines' => $routines,
- *     );
- *     return array('your_module_name' => $upgrade);
+ *   function foo($bar) { // Stand-alone.
+ *     if ($bar) {
+ *       // Do something.
+ *     }
+ *   }
+ *   class example {
+ *     function foo($bar) { // Embedded.
+ *       if ($bar) {
+ *         // Do something.
+ *       }
+ *     }
  *   }
  * @endcode
  *
- * The naming convention for a conversion routine is:
- * @code
- *   your_module_name_convert_your_routine_name();
- * @endcode
+ * Coder Upgrade will call this alter hook for each hook function in the file
+ * that was parsed. However, the function name must follow the naming convention
+ * for a hook, i.e, your_module_name_hook. If your module declares a hook for
+ * another module or otherwise digresses from the standard naming convention,
+ * then you will need to add a converion routine to the list to be able to alter
+ * this function.
  *
- * The array returned by your_module_name_conversions_list() should be a list
- * of the suffixes of your conversion routines. The suffix is the portion of
- * the routine name following "_convert_" above.
+ * Refer to the grammar parser documentation for details of the function object
+ * (i.e. PGPClass).
  *
- * Group changes by processing order (beginning, middle, or end). The beginning
- * and end conversion routines are applied only at the module or directory
- * level and the routine will need to determine what to do and how (regex or
- * parser) to do it.
+ * @see hook_upgrade_file_alter
+ * @see PGPClass
  *
- * The middle changes are applied at the file level and the list entry must
- * specify the handler (regex or parser). The regex changes are applied before
- * the parser changes.
- * @code
- *   function your_module_name_conversions_list() {
- *     $list = array(
- *       'beginning' => array(),
- *       'middle' => array(),
- *       'end' => array(),
- *     );
- *
- *     $list['middle'] = array(
- *       array(
- *         'name' => 'your_routine_name_regex',
- *         'type' => 'regex',
- *         'file' => 'your_module_name.regex_conversions.inc',
- *       ),
- *       array(
- *         'name' => 'your_routine_name_parser',
- *         'type' => 'parser',
- *         'file' => 'your_module_name.parser_conversions.inc',
- *       ),
- *     );
- *
- *     return $list;
- *   }
- * @endcode
- *
- * The $file parameter passed to your regex conversion routine is a reference
- * to the text of the code file to be upgraded. Be sure to include an ampersand
- * before $file so that your changes will be incorporated into the output code
- * file.
- * @code
- *   function your_module_name_convert_your_routine_name_regex(&$file) {
- *     // Do something to $file.
- *   }
- * @endcode
- *
- * The $reader parameter passed to your parser conversion routine is a
- * reference to the object containing the grammar statements of the file to
- * convert. In PHP5, an ampersand is not needed before $reader as objects are
- * automatically passed by reference.
- * @code
- *   function your_module_name_convert_your_routine_name_parser(&$reader) {
- *     // Do something with $reader.
- *   }
- * @endcode
- *
- * @param boolean $include_routines
- *   Indicates whether to include the list of conversion routines. This list
- *   is only needed when the conversions are to be applied (typically on form
- *   submission). This parameter equals FALSE when this hook is invoked by
- *   Coder Upgrade while building the form, and TRUE when applying the
- *   conversions.
+ * @param PGPNode $node
+ *   A node object containing a PGPClass (or function) item.
+ * @param PGPReader $reader
+ *   The object containing the grammar statements of the file to convert.
+ * @param $hook
+ *   The name of the function (excluding the module name).
  */
-function hook_upgrades($include_routines)  {
-  $routines = $include_routines ? your_module_name_conversions_list() : array();
-  $upgrade = array(
-    'title' => t('Your module API changes from 6.x to 7.x'),
-    'link' => 'http://...',
-    'routines' => $routines,
-  );
-  return array('your_module_name' => $upgrade);
+function hook_upgrade_hook_alter(&$node, &$reader, &$hook) {
+  global $_coder_upgrade_module_name;
+
+  // Get the function object.
+  $item = &$node->data;
+
+  // Modify hook function.
+  switch ($hook) {
+    case 'old_hook_name':
+      // Rename the function.
+      $item->name = $_coder_upgrade_module_name . '_new_hook_name';
+      // Update the document comment.
+      $item->comment['value'] = preg_replace('@\* Implement\s+@', "* Implements ", $item->comment['value']);
+
+      if ($item->parameters->count() > 1) {
+        // Switch the first two parameters.
+        $p0 = $item->getParameter(0);
+        $p1 = $item->getParameter(1);
+        $item->setParameter(0, $p1);
+        $item->setParameter(1, $p0);
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+/**
+ * Alters a code file using the grammar parser.
+ *
+ * This hook allows contributed modules to alter a code file object using the
+ * grammar parser. If a module defines a class, then the calls to its methods
+ * are not included in the calls to hook_upgrade_hook_HOOK_NAME_alter or
+ * hook_upgrade_hook_alter.
+ *
+ * @param PGPReader $reader
+ *   The object containing the grammar statements of the file to convert.
+ */
+function hook_upgrade_file_alter(&$reader) {
+  /*
+   * Task: Modify calls to class methods.
+   */
+
+  // Get list of function calls (including the calls to class methods).
+  $items = &$reader->getFunctionCalls();
+  // Loop on list.
+  foreach ($items as &$item) {
+    if (!isset($item) || !is_object($item) || !is_a($item, 'PGPFunctionCall') || $item->type != T_FUNCTION_CALL) {
+      /*
+       * These checks are necessary as the reference (i.e. $item) could have
+       * been changed in another routine so that it no longer refers to a
+       * function call object.
+       */
+      continue;
+    }
+
+    /*
+     * To be a call to a class method, the function name must be an expression
+     * like $this->foo() as opposed to a string or a single variable. This code
+     * checks the name is an expression (using is_a($item->name, 'PGPOperand'))
+     * and the value element of the name object is '$this'.
+     *
+     * Review the grammar structure object using $item->print_r().
+     */
+    if (is_a($item->name, 'PGPOperand') && $item->name->findNode('value') == '$this') {
+      // Strip '$this->' from the name.
+      $name = substr($item->name->toString(), 7);
+      // Modify the function call
+      my_module_convert_method($item, $reader, $name);
+    }
+  }
+}
+
+/**
+ * Alters an install file using the grammar parser.
+ *
+ * This hook allows contributed modules to alter an install file object using
+ * the grammar parser. This hook allows for segregation of upgrade routines that
+ * only apply to an install file (e.g. the database schema API).
+ *
+ * @see hook_upgrade_file_alter
+ * @see hook_upgrade_hook_HOOK_NAME_alter
+ * @see hook_upgrade_hook_alter
+ *
+ * @param PGPReader $reader
+ *   The object containing the grammar statements of the file to convert.
+ */
+function hook_upgrade_parser_install_alter(&$reader) {
+  // Do something to the file.
+}
+
+/**
+ * Processes the directory before upgrade routines are applied.
+ *
+ * This hook can be used to cache information needed by other routines. For
+ * example, core changes need to know about hook_theme or hook_menu to make
+ * theme changes and form API changes.
+ *
+ * @param string $dirname
+ *   The name of the directory with the original files.
+ */
+function hook_upgrade_begin_alter($dirname) {
+  // Do something.
+}
+
+/**
+ * Processes the directory after upgrade routines are applied.
+ *
+ * This hook can be used to apply finishing touches to the directory of
+ * converted files. For example, a D7 core change is to add file entries to
+ * the .info files.
+ *
+ * @param string $dirname
+ *   The name of the directory with the converted files.
+ */
+function hook_upgrade_end_alter($dirname) {
+  // Do something.
+}
+
+/**
+ * Alters the text of a code file using regular expressions.
+ *
+ * @param string $file
+ *   The text of the file to convert.
+ */
+function hook_upgrade_regex_alter(&$file) {
+  $hook = 'your_changes'; // Used as the label in the log file.
+  $cur = $file;
+  $new = $cur;
+
+  $from = array();
+  $to = array();
+
+  // Do something to $file.
+  $from[] = '/(your_module_name)/';
+  $to[] = "$1";
+
+  coder_upgrade_do_conversions($from, $to, $new);
+  coder_upgrade_save_changes($cur, $new, $file, $hook);
+}
+
+/**
+ * Alters the text of an info file using regular expressions.
+ *
+ * @param string $file
+ *   The text of the file to convert.
+ */
+function hook_upgrade_regex_info_alter(&$file) {
+  $hook = 'info_file'; // Used as the label in the log file.
+  $cur = $file;
+  $new = $cur;
+
+  $from = array();
+  $to = array();
+
+  // Info file should specify core = 7.x.
+  $from[] = '@^core\s+=\s+.*?$@m';
+  $to[] = 'core = 7.x';
+
+  coder_upgrade_do_conversions($from, $to, $new);
+  coder_upgrade_save_changes($cur, $new, $file, $hook);
+}
+
+/**
+ * Alters the text of an install file using regular expressions.
+ *
+ * @param string $file
+ *   The text of the file to convert.
+ */
+function hook_upgrade_regex_install_alter(&$file) {
+  $hook = 'info_file'; // Used as the label in the log file.
+  $cur = $file;
+  $new = $cur;
+
+  $from = array();
+  $to = array();
+
+  // Do something to $file.
+  $from[] = '/(your_module_name)/';
+  $to[] = "$1";
+
+  coder_upgrade_do_conversions($from, $to, $new);
+  coder_upgrade_save_changes($cur, $new, $file, $hook);
 }
 
 /**
@@ -229,7 +430,21 @@ function hook_upgrades($include_routines)  {
  */
 
 /**
- * Implements hook_upgrade_call_function_name_alter().
+ * Implements hook_upgrade_info().
+ */
+function your_module_name_upgrade_info() {
+  $upgrade = array(
+    'title' => t('Your module API changes from 6.x to 7.x'),
+    'link' => 'http://...',
+    'files' => array(
+      'upgrades/your_module_name.upgrade',
+    ),
+  );
+  return array('your_module_name' => $upgrade);
+}
+
+/**
+ * Implements hook_upgrade_call_FUNCTION_NAME_alter().
  */
 function your_module_name_upgrade_call_function_name_alter(&$item, &$reader) {
   // Change the function name.
@@ -242,7 +457,7 @@ function your_module_name_upgrade_call_function_name_alter(&$item, &$reader) {
 }
 
 /**
- * Implements hook_upgrade_hook_hook_name_alter().
+ * Implements hook_upgrade_hook_HOOK_NAME_alter().
  */
 function your_module_name_upgrade_hook_hook_name_alter(&$node, &$reader) {
   global $_coder_upgrade_module_name;
@@ -265,61 +480,9 @@ function your_module_name_upgrade_hook_hook_name_alter(&$node, &$reader) {
 }
 
 /**
- * Implements hook_upgrades().
+ * Implements hook_upgrade_regex_alter().
  */
-function your_module_name_upgrades($include_routines)  {
-  $routines = $include_routines ? your_module_name_conversions_list() : array();
-  $upgrade = array(
-    'title' => t('Your module API changes from 6.x to 7.x'),
-    'link' => 'http://...',
-    'routines' => $routines,
-  );
-  return array('your_module_name' => $upgrade);
-}
-
-/**
- * Returns a list of conversion routine suffixes.
- *
- * The complete function name that Coder Upgrade searches for is:
- *   <your_module_name> . '_convert_' . <your_routine_name>
- *
- * The entries in the array returned from this routine should only contain
- * the <your_routine_name> portion.
- *
- * @return array
- *   Array of conversion routine suffixes.
- */
-function your_module_name_conversions_list() {
-  $list = array(
-    'beginning' => array(),
-    'middle' => array(),
-    'end' => array(),
-  );
-
-  $list['middle'] = array(
-    array(
-      'name' => 'your_routine_name_regex',
-      'type' => 'regex',
-      'file' => 'your_module_name.regex_conversions.inc',
-    ),
-    array(
-      'name' => 'your_routine_name_parser',
-      'type' => 'parser',
-      'file' => 'your_module_name.parser_conversions.inc',
-    ),
-  );
-
-  return $list;
-}
-
-/**
- * Describe the upgrade applied by this routine.
- *
- * @param string $file
- *   The text of the file to convert.
- */
-function your_module_name_convert_your_routine_name_regex(&$file) {
-  // Do something to $file.
+function your_module_name_upgrade_regex_alter(&$file) {
   $hook = 'your_changes'; // Used as the label in the log file.
   $cur = $file;
   $new = $cur;
@@ -327,6 +490,7 @@ function your_module_name_convert_your_routine_name_regex(&$file) {
   $from = array();
   $to = array();
 
+  // Do something to $file.
   $from[] = '/(your_module_name)/';
   $to[] = "$1";
 
@@ -335,11 +499,20 @@ function your_module_name_convert_your_routine_name_regex(&$file) {
 }
 
 /**
- * Describe the upgrade applied by this routine.
- *
- * @param PGPReader $reader
- *   The object containing the grammar statements of the file to convert.
+ * Implements hook_upgrade_regex_info_alter().
  */
-function your_module_name_convert_your_routine_name_parser(&$reader) {
-  // Do something with $reader.
+function your_module_name_upgrade_regex_info_alter(&$file) {
+  $hook = 'info_file';
+  $cur = $file;
+  $new = $cur;
+
+  $from = array();
+  $to = array();
+
+  // Info file should specify core = 7.x.
+  $from[] = '@^core\s+=\s+.*?$@m';
+  $to[] = 'core = 7.x';
+
+  coder_upgrade_do_conversions($from, $to, $new);
+  coder_upgrade_save_changes($cur, $new, $file, $hook);
 }
