e178080edd1a26602a86cb1301b58b578cb4ce3d
Klaus Purer <klaus.purer@gmail.com>
2011-11-11 14:35:52 +0100

    Simplified string concatenation operator rule.

--- DrupalCodingStandard/Sniffs/Strings/ConcatenationSpacingSniff.php
+++ DrupalCodingStandard/Sniffs/Strings/ConcatenationSpacingSniff.php
@@ -39,54 +39,15 @@ class DrupalCodingStandard_Sniffs_Strings_ConcatenationSpacingSniff implements P
 
     /**
      * Processes this test, when one of its tokens is encountered.
-     *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the
-     *                                        stack passed in $tokens.
-     *
-     * @return void
      */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
         $tokens = $phpcsFile->getTokens();
-
-        $found    = '';
-        $expected = '';
-        $error    = false;
-
-        if ($tokens[($stackPtr - 1)]['code'] !== T_WHITESPACE) {
-            $expected .= '...'.substr($tokens[($stackPtr - 2)]['content'], -5).$tokens[$stackPtr]['content'];
-            $found    .= '...'.substr($tokens[($stackPtr - 2)]['content'], -5).$tokens[($stackPtr - 1)]['content'].$tokens[$stackPtr]['content'];
-            $error     = true;
-        } else {
-            $found    .= '...'.substr($tokens[($stackPtr - 1)]['content'], -5).$tokens[$stackPtr]['content'];
-            $expected .= '...'.substr($tokens[($stackPtr - 1)]['content'], -5).$tokens[$stackPtr]['content'];
+        if ($tokens[($stackPtr - 1)]['code'] !== T_WHITESPACE || $tokens[($stackPtr + 1)]['code'] !== T_WHITESPACE) {
+            $message = 'Concat operator must be surrounded by spaces';
+            $phpcsFile->addError($message, $stackPtr, 'Missing');
         }
-
-        if ($tokens[($stackPtr + 1)]['code'] !== T_WHITESPACE) {
-            $expected .= substr($tokens[($stackPtr + 2)]['content'], 0, 5).'...';
-            $found    .= $tokens[($stackPtr + 1)]['content'].substr($tokens[($stackPtr + 2)]['content'], 0, 5).'...';
-            $error     = true;
-        } else {
-            $found    .= $tokens[($stackPtr + 1)]['content'];
-            $expected .= $tokens[($stackPtr + 1)]['content'];
-        }
-
-        if ($error === true) {
-            $found    = str_replace("\r\n", '\n', $found);
-            $found    = str_replace("\n", '\n', $found);
-            $found    = str_replace("\r", '\n', $found);
-            $expected = str_replace("\r\n", '\n', $expected);
-            $expected = str_replace("\n", '\n', $expected);
-            $expected = str_replace("\r", '\n', $expected);
-
-            $message = "Concat operator must be surrounded by spaces. Found \"$found\"; expected \"$expected\"";
-            $phpcsFile->addError($message, $stackPtr);
-        }
-
-    }//end process()
-
-
+    }
 }//end class
 
 ?>
