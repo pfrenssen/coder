83c46e1efbd56e352a9f67f3a83edbaa80632af6
Klaus Purer <klaus.purer@gmail.com>
2011-12-08 18:13:15 +0100

    Improved end of file sniff, now also works on TXT and other text files.

--- DrupalCodingStandard/Sniffs/WhiteSpace/EmptyLinesSniff.php
+++ DrupalCodingStandard/Sniffs/WhiteSpace/EmptyLinesSniff.php
@@ -42,7 +42,11 @@ class DrupalCodingStandard_Sniffs_WhiteSpace_EmptyLinesSniff implements PHP_Code
      */
     public function register()
     {
-        return array(T_WHITESPACE);
+        return array(
+                T_WHITESPACE,
+                T_CLOSE_TAG,
+                T_INLINE_HTML,
+               );
 
     }//end register()
 
@@ -72,16 +76,57 @@ class DrupalCodingStandard_Sniffs_WhiteSpace_EmptyLinesSniff implements PHP_Code
             $phpcsFile->addError($error, $stackPtr + 3, 'EmptyLines');
         }
 
-        $nextWhiteSpace = $phpcsFile->findNext(T_WHITESPACE, $stackPtr + 1);
-        // Check if this is the last white space in the file.
-        if ($nextWhiteSpace === false) {
-            $notLastToken = count($tokens) !== $stackPtr + 1;
-            $notEOLChar   = $tokens[$stackPtr]['content'] !== $phpcsFile->eolChar;
-            $moreEOL      = isset($tokens[$stackPtr - 1]) && $tokens[$stackPtr - 1]['content'] === $phpcsFile->eolChar;
-            if ($notLastToken === true || $notEOLChar === true || $moreEOL === true) {
-                $error = 'Files must end in a single new line character';
-                $phpcsFile->addError($error, $stackPtr, 'EmptyLinesFileEnd');
-            }
+        $error     = false;
+        $token     = $tokens[$stackPtr];
+        $lastToken = count($tokens) === ($stackPtr + 1);
+        switch ($token['code']) {
+            case T_WHITESPACE:
+                $nextWhiteSpace = $phpcsFile->findNext(T_WHITESPACE, $stackPtr + 1);
+                $nextCloseTag   = $phpcsFile->findNext(T_CLOSE_TAG, $stackPtr + 1);
+                // Check if this is the last white space in the file.
+                if ($nextWhiteSpace === false && $nextCloseTag === false) {
+                    // There has to be a white space at the end.
+                    if ($lastToken === false) {
+                        $error = true;
+                        break;
+                    }
+                    // The only white space allowed is the \n character.
+                    if ($token['content'] !== $phpcsFile->eolChar) {
+                        $error = true;
+                        break;
+                    }
+                    // Only one new line character is allowed a t the end.
+                    if (isset($tokens[$stackPtr - 1]) === true && $tokens[$stackPtr - 1]['content'] === $phpcsFile->eolChar) {
+                        $error = true;
+                        break;
+                    }
+                }
+                break;
+            case T_CLOSE_TAG:
+                // The close tag must end in a \n if it is the last token in the file.
+                if ($lastToken === true && substr($token['content'], -1) !== $phpcsFile->eolChar) {
+                    $error = true;
+                }
+                break;
+            case T_INLINE_HTML:
+                if ($lastToken === true) {
+                    // The last line has to end with a \n.
+                    if (substr($token['content'], -1) !== $phpcsFile->eolChar) {
+                        $error = true;
+                        break;
+                    }
+                    // Blank lines at the end are not allowed.
+                    if ($token['content'] === $phpcsFile->eolChar) {
+                        $error = true;
+                        break;
+                    }
+                }
+                break;
+        }//end switch
+
+        if ($error === true) {
+            $error = 'Files must end in a single new line character';
+            $phpcsFile->addError($error, $stackPtr, 'EmptyLinesFileEnd');
         }
 
     }//end process()
