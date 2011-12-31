857b7cc52c00b258b6699c1062dda77e0e3de382
Klaus Purer <klaus.purer@gmail.com>
2011-12-31 19:23:23 +0100

    Improved info file sniff to use the PHP_CodeSniffer_MultiFileSniff interface.

--- DrupalCodingStandard/Sniffs/InfoFiles/ClassFilesSniff.php
+++ DrupalCodingStandard/Sniffs/InfoFiles/ClassFilesSniff.php
@@ -17,77 +17,77 @@
  * @package  PHP_CodeSniffer
  * @link     http://pear.php.net/package/PHP_CodeSniffer
  */
-class DrupalCodingStandard_Sniffs_InfoFiles_ClassFilesSniff implements PHP_CodeSniffer_Sniff
+class DrupalCodingStandard_Sniffs_InfoFiles_ClassFilesSniff implements PHP_CodeSniffer_MultiFileSniff
 {
 
 
     /**
-     * Returns an array of tokens this test wants to listen for.
+     * Called once per script run to allow for processing of this sniff.
      *
-     * @return array
-     */
-    public function register()
-    {
-        return array(T_INLINE_HTML);
-
-    }//end register()
-
-
-    /**
-     * Processes this test, when one of its tokens is encountered.
-     *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token
-     *                                        in the stack passed in $tokens.
+     * @param array(PHP_CodeSniffer_File) $files The PHP_CodeSniffer files processed
+     *                                           during the script run.
      *
      * @return void
      */
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    public function process(array $files)
     {
-        $fileExtension = strtolower(substr($phpcsFile->getFilename(), -4));
-        // Execute only on *.info files and only once.
-        if ($fileExtension === 'info' && $stackPtr === 0) {
-            $tokens = $phpcsFile->getTokens();
-            // @todo: make use of parse_ini_string() (since PHP 5.3) as soon as
-            // PHP 5.2 is really dead.
-            $info = parse_ini_file($phpcsFile->getFilename());
-            if (isset($info['files']) === true && is_array($info['files']) === true) {
-                foreach ($info['files'] as $file) {
-                    $fileName = dirname($phpcsFile->getFilename()).'/'.$file;
-                    if (file_exists($fileName) === true) {
-                        $contents = file_get_contents($fileName);
-                    } else {
-                        $contents = '';
-                    }
-
-                    // Tokenize the referenced file to check if it contains a
-                    // class/interface token.
-                    $fileTokens = token_get_all($contents);
-                    $found      = false;
-                    foreach ($fileTokens as $token) {
-                        if (isset($token[0]) === true && ($token[0] === T_CLASS || $token[0] === T_INTERFACE)) {
-                            $found = true;
-                            break;
+        foreach ($files as $phpcsFile) {
+            // Execute only on *.info files.
+            $fileExtension = strtolower(substr($phpcsFile->getFilename(), -4));
+            if ($fileExtension === 'info') {
+                $info = parse_ini_file($phpcsFile->getFilename());
+                if (isset($info['files']) === true && is_array($info['files']) === true) {
+                    foreach ($info['files'] as $file) {
+                        $fileName = dirname($phpcsFile->getFilename()).'/'.$file;
+                        if (file_exists($fileName) === false) {
+                            // We need to find the position of the offending line in the
+                            // info file.
+                            $ptr   = $this->getPtr($file, $phpcsFile);
+                            $error = 'Declared file was not found';
+                            $phpcsFile->addError($error, $ptr, 'DeclaredFileNotFound');
+                            continue;
                         }
-                    }
 
-                    if ($found === false) {
-                        // We need to find the position of the offending line in the
-                        // info file.
-                        foreach ($tokens as $ptr => $tokenInfo) {
-                            if (strpos($tokenInfo['content'], $file) !== false) {
-                                break;
+                        foreach ($files as $searchFile) {
+                            if ($searchFile->getFilename() === $fileName) {
+                                $stackPtr = $searchFile->findNext(array(T_CLASS, T_INTERFACE), 0);
+                                if ($stackPtr === false) {
+                                    $ptr   = $this->getPtr($file, $phpcsFile);
+                                    $error = "It's only necessary to declare files[] if they declare a class or interface.";
+                                    $phpcsFile->addError($error, $ptr, 'UnecessaryFileDeclaration');
+                                }
+
+                                continue 2;
                             }
                         }
-
-                        $error = "It's only necessary to declare files[] if they declare a class or interface.";
-                        $phpcsFile->addError($error, $ptr, 'UnecessaryFileDeclaration');
-                    }
-                }//end foreach
+                    }//end foreach
+                }//end if
             }//end if
-        }//end if
+        }//end foreach
 
     }//end process()
+
+
+    /**
+     * Helper function that returns the position of the file name in the info file.
+     *
+     * @param string               $fileName File name to search for.
+     * @param PHP_CodeSniffer_File $infoFile Info file to search in.
+     *
+     * @return int|false Returns the stack position if the file name is found, false
+     * 									 otherwise.
+     */
+    protected function getPtr($fileName, PHP_CodeSniffer_File $infoFile)
+    {
+        foreach ($infoFile->getTokens() as $ptr => $tokenInfo) {
+            if (strpos($tokenInfo['content'], $fileName) !== false) {
+                return $ptr;
+            }
+        }
+
+        return false;
+
+    }//end getPtr()
 
 
 }//end class
