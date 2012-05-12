fda9b1dfda15437c15cfac4fefb66b411627ce9f
TravisCarden <TravisCarden@236758.no-reply.drupal.org>
2012-05-12 15:10:13 +0200

    Issue #1535558 by TravisCarden: Fixed a missing core global variable name.

--- Drupal/Sniffs/NamingConventions/ValidGlobalSniff.php
+++ Drupal/Sniffs/NamingConventions/ValidGlobalSniff.php
@@ -46,6 +46,7 @@ class Drupal_Sniffs_NamingConventions_ValidGlobalSniff implements PHP_CodeSniffe
                            '$items',
                            '$language',
                            '$language_content',
+                           '$language_interface',
                            '$language_url',
                            '$locks',
                            '$menu_admin',
