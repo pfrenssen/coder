5d52bcfa6c2a81940e1c5a0f3d332e6502c8d134
sun <sun@unleashedmind.com>
2012-07-14 20:19:19 +0200

    Fixed needless working directory change in Windows command script.

--- scripts/coder_format/coder_format.cmd
+++ scripts/coder_format/coder_format.cmd
@@ -7,20 +7,13 @@ set coderFormatPath=c:\program files\coder_format
 :: ----- You should not need to edit anything below. ----- ::
 
 if "%~1"=="" goto error
-set oldpwd=%CD%
-cd /d "%coderFormatPath%"
-
 rem Check if at least one argument follows a --undo parameter.
 if "%~1"=="--undo" if "%~2"=="" goto error
 
-start "coder_format" /D "%coderFormatPath%" /B /WAIT php coder_format.php %*
-goto done
+call php "%coderFormatPath%\coder_format.php" %*
+goto :EOF
 
 :error
 echo ERROR: Wrong arguments supplied. Please see usage in coder_format.php.
 goto :EOF
-
-:done
-rem Jump back to original working directory.
-cd /d "%oldpwd%"
 
