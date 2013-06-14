30a9958d388b49e3d9106f4e62a86bd9d64b4d88
Klaus Purer <klaus.purer@gmail.com>
2013-06-14 15:06:41 +0200

    Added a sniff for untranslated #options array values.

---
+++ DrupalPractice/Sniffs/General/OptionsTSniff.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * DrupalPractice_Sniffs_General_OptionsTSniff
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Checks that values in #otions form arrays are translated.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+class DrupalPractice_Sniffs_General_OptionsTSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_CONSTANT_ENCAPSED_STRING);
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The current file being processed.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        // Look for the sting "#options".
+        $tokens = $phpcsFile->getTokens();
+        if ($tokens[$stackPtr]['content'] !== '"#options"' && $tokens[$stackPtr]['content'] !== "'#options'") {
+            return;
+        }
+
+        // Look for an opening array pattern that starts to define #options
+        // values.
+        $statementEnd = $phpcsFile->findNext(T_SEMICOLON, ($stackPtr + 1));
+        $arrayString  = $phpcsFile->getTokensAsString(($stackPtr + 1), ($statementEnd - $stackPtr));
+        // Cut out all the white space.
+        $arrayString = preg_replace('/\s+/', '', $arrayString);
+
+        if (strpos($arrayString, '=>array(') !== 0 && strpos($arrayString, ']=array(') !== 0) {
+            return;
+        }
+
+        // Go through the array by examining stuff after "=>".
+        $arrow = $phpcsFile->findNext(T_DOUBLE_ARROW, ($stackPtr + 1), null, false, null, true);
+        while ($arrow !== false) {
+            $arrayValue = $phpcsFile->findNext(T_WHITESPACE, ($arrow + 1), null, true);
+            // We are only interested in string literals.
+            if ($tokens[$arrayValue]['code'] === T_CONSTANT_ENCAPSED_STRING) {
+                // We need to make sure that the string is the one and only part
+                // of the array value.
+                $afterValue = $phpcsFile->findNext(T_WHITESPACE, ($arrayValue + 1), null, true);
+                if ($tokens[$afterValue]['code'] === T_COMMA || $tokens[$afterValue]['code'] === T_CLOSE_PARENTHESIS) {
+                    $warning = '#options values usually have to run through t() for translation';
+                    $phpcsFile->addWarning($warning, $arrayValue, 'TforValue');
+                }
+            }
+
+            $arrow = $phpcsFile->findNext(T_DOUBLE_ARROW, ($arrow + 1), null, false, null, true);
+        }
+
+    }//end process()
+
+
+}//end class
+
+?>
