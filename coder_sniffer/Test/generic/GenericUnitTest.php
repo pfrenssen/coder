4d30682798cc05830dc4f5ffdae904af47fdfa23
Klaus Purer <klaus.purer@gmail.com>
2014-02-23 12:26:34 +0100

    Added test case for rules referenced from the generic coding standard.

---
+++ coder_sniffer/Test/generic/GenericUnitTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Unit test class for sniffs inherited from the Generic standard.
+ */
+
+/**
+ * Unit test class for sniffs inherited from the Generic standard.
+ */
+class Drupal_GenericUnitTest extends CoderSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList()
+    {
+        return array(
+                1 => 1,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array();
+
+    }//end getWarningList()
+
+    /**
+     * Returns a list of test files that should be checked.
+     *
+     * @return array The list of test files.
+     */
+    protected function getTestFiles() {
+        return array(dirname(__FILE__) . '/GenericUnitTest.inc');
+    }
+
+    /**
+     * Returns a list of sniff codes that should be checked in this test.
+     *
+     * @return array The list of sniff codes.
+     */
+    protected function getSniffCodes() {
+        $ruleset = simplexml_load_file(dirname(__FILE__) . '/../../Drupal/ruleset.xml');
+        $codes = array();
+
+        // We only want referenced rules from the "Generic" standard.
+        foreach($ruleset->rule as $rule) {
+            $name = (string) $rule['ref'];
+            if (strpos($name, 'Generic') === 0) {
+                $codes[] = $name;
+            }
+        }
+        return $codes;
+    }
+
+
+}//end class
+
+?>
