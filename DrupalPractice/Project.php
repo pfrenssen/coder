356416e864e23ea947471d46ddc7adba32208b63
Klaus Purer <klaus.purer@gmail.com>
2014-06-28 15:02:49 +0200

    Implemented Drupal core version detection from info files or info.yml files.

--- DrupalPractice/Project.php
+++ DrupalPractice/Project.php
@@ -45,10 +45,43 @@ class DrupalPractice_Project
             return $pathParts['filename'];
         }
 
+        $infoFile = static::getInfoFile($phpcsFile);
+        if ($infoFile === false) {
+            return false;
+        }
+        $pathParts = pathinfo($infoFile);
+        $cache[$phpcsFile->getFilename()] = $pathParts['filename'];
+        return $pathParts['filename'];
+
+    }//end getName()
+
+
+    /**
+     * Determines the info file a file might be associated with.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     *
+     * @return string|false The project info file name or false if it could not
+     *   be derived.
+     */
+    public static function getInfoFile(PHP_CodeSniffer_File $phpcsFile)
+    {
+        // Cache the project name per file as this might get called often.
+        static $cache;
+
+        if (isset($cache[$phpcsFile->getFilename()]) === true) {
+            return $cache[$phpcsFile->getFilename()];
+        }
+
+        $pathParts = pathinfo($phpcsFile->getFilename());
+
         // Search for an info file.
         $dir = $pathParts['dirname'];
         do {
-            $infoFiles = glob("$dir/*.info");
+            $infoFiles = glob("$dir/*.info.yml");
+            if (empty($infoFiles) === true) {
+                $infoFiles = glob("$dir/*.info");
+            }
             // Go one directory up if we do not find an info file here.
             $dir = dirname($dir);
         } while (empty($infoFiles) && $dir != dirname($dir));
@@ -62,11 +95,9 @@ class DrupalPractice_Project
         // Sort the info file names and take the shortest info file.
         usort($infoFiles, array('DrupalPractice_Project', 'compareLength'));
         $infoFile = $infoFiles[0];
-        $pathParts = pathinfo($infoFile);
-        $cache[$phpcsFile->getFilename()] = $pathParts['filename'];
-        return $pathParts['filename'];
-
-    }//end getName()
+        $cache[$phpcsFile->getFilename()] = $infoFile;
+        return $infoFile;
+    }
 
 
     /**
@@ -79,6 +110,35 @@ class DrupalPractice_Project
     }//end compareLength()
 
 
-}//end class
+    /**
+     * Determines the Drupal core version a file might be associated with.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     *
+     * @return string|false The core version string or false if it could not
+     *   be derived.
+     */
+    public static function getCoreVersion(PHP_CodeSniffer_File $phpcsFile)
+    {
+        $infoFile = static::getInfoFile($phpcsFile);
+        if ($infoFile === false) {
+            return false;
+        }
+        $pathParts = pathinfo($infoFile);
 
-?>
+        // Drupal 6 and 7 use the .info file extension.
+        if ($pathParts['extension'] === 'info') {
+            $info_settings = Drupal_Sniffs_InfoFiles_ClassFilesSniff::drupalParseInfoFormat(file_get_contents($infoFile));
+            if (isset($info_settings['core'])) {
+                return $info_settings['core'];
+            }
+        } else {
+            // Drupal 8 uses the .yml file extension.
+            // @todo Revisit for Drupal 9, but I don't want to do YAML parsing
+            // for now.
+            return '8.x';
+        }
+    }//end getCoreVersion()
+
+
+}//end class
