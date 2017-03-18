984c54a7b1e8f27ff1c32348df69712afd86b17f
Klaus Purer <klaus.purer@gmail.com>
2017-03-18 11:28:49 +0100

    feat(ExpectedExceptionSniff): Add sniff to forbid usage of PHPUnit @expectedException annotation (#2859286)

---
+++ coder_sniffer/DrupalPractice/Test/Commenting/ExpectedExceptionUnitTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class DrupalPractice_Sniffs_Commenting_ExpectedExceptionUnitTest extends CoderSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    protected function getErrorList($testFile)
+    {
+        return array();
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    protected function getWarningList($testFile)
+    {
+        return array(
+                8 => 1,
+                9 => 1,
+                10 => 1,
+                11 => 1,0
+               );
+
+    }//end getWarningList()
+
+
+}//end class
