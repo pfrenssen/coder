6243cb3c4298404bd47d8bfb0802d637e3cf077b
alexpott <alex.a.pott@gmail.com>
2019-04-14 10:31:26 -0700

    feat(GenderNeutralComment): Add sniff to check for gendered words like he/him in comments (#3021632 #20)

---
+++ coder_sniffer/Drupal/Test/Commenting/GenderNeutralCommentUnitTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\Sniffs\Commenting;
+
+use Drupal\Test\CoderSniffUnitTest;
+
+class GenderNeutralCommentUnitTest extends CoderSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList($testFile = NULL)
+    {
+        return array(
+            7 => 1,
+            8 => 1,
+            12 => 1,
+            13 => 1,
+            14 => 1,
+            16 => 1,
+            17 => 1,
+            19 => 1,
+            20 => 1,
+            21 => 1,
+            22 => 1,
+            26 => 1,
+            27 => 1,
+            31 => 1,
+           );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList($testFile = NULL)
+    {
+        return array(
+               );
+
+    }//end getWarningList()
+
+
+}//end class
