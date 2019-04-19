6e88ebe84e4b823568137d609b0da55e03620397
Mircea Fernea <mircea.fernea@gmail.com>
2019-04-19 19:49:02 +0300

    fix(RemoteAddress): Improve error message for Drupal 8 and prevent errors on assignment ( #2902547)

---
+++ coder_sniffer/Drupal/Test/Semantics/RemoteAddressUnitTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\Sniffs\Semantics;
+
+use Drupal\Test\CoderSniffUnitTest;
+
+class RemoteAddressUnitTest extends CoderSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile The filename of the test file.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList($testFile = NULL)
+    {
+        return array(
+                12 => 1,
+                19 => 1,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile The filename of the test file.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList($testFile = NULL)
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
