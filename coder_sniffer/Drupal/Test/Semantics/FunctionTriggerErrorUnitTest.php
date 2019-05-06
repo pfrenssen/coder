34fcfc8a4ee0dff234b0d40aa8f2a44acd180121
Jonathan Smith <drupal@sandfordsolutions.com>
2019-05-06 16:48:38 +0100

    new syntax standards in funtionTriggerErrorUnitTest.php

--- coder_sniffer/Drupal/Test/Semantics/FunctionTriggerErrorUnitTest.php
+++ coder_sniffer/Drupal/Test/Semantics/FunctionTriggerErrorUnitTest.php
@@ -14,26 +14,28 @@ class FunctionTriggerErrorUnitTest extends CoderSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of errors that should occur on that line.
      *
+     * @param string $testFile The name of the file being tested.
+     *
      * @return array(int => int)
      */
-    public function getErrorList()
+    public function getErrorList($testFile=null)
     {
         switch ($testFile) {
-            case 'FunctionTriggerErrorUnitTest.1.inc':
-                return [
-                    30 => 1,
-                    32 => 1,
-                    34 => 1,
-                    36 => 1,
-                    38 => 1,
-                    40 => 1,
-                ];
-            case 'FunctionTriggerErrorUnitTest.2.inc':
-                return [29 => 1];
-            case 'FunctionTriggerErrorUnitTest.3.inc':
-                return [14 => 1];
-            default:
-                return [];
+        case 'FunctionTriggerErrorUnitTest.1.inc':
+            return [
+                30 => 1,
+                32 => 1,
+                34 => 1,
+                36 => 1,
+                38 => 1,
+                40 => 1,
+            ];
+        case 'FunctionTriggerErrorUnitTest.2.inc':
+            return [29 => 1];
+        case 'FunctionTriggerErrorUnitTest.3.inc':
+            return [14 => 1];
+        default:
+            return [];
         }
 
     }//end getErrorList()
@@ -45,32 +47,34 @@ class FunctionTriggerErrorUnitTest extends CoderSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of warnings that should occur on that line.
      *
+     * @param string $testFile The name of the file being tested.
+     *
      * @return array(int => int)
      */
-    public function getWarningList()
+    public function getWarningList($testFile=null)
     {
         switch ($testFile) {
-            case 'FunctionTriggerErrorUnitTest.1.inc':
-                return [
-                    42 => 1,
-                    44 => 1,
-                    46 => 1,
-                    48 => 1,
-                    50 => 1,
-                    52 => 1,
-                    54 => 1,
-                    56 => 1,
-                    58 => 1,
-                    60 => 1,
-                    62 => 1,
-                    64 => 1,
-                    66 => 1,
-                ];
-            case 'FunctionTriggerErrorUnitTest.2.inc':
-            case 'FunctionTriggerErrorUnitTest.3.inc':
-            default:
-                return [];
-        }
+        case 'FunctionTriggerErrorUnitTest.1.inc':
+            return [
+                42 => 1,
+                44 => 1,
+                46 => 1,
+                48 => 1,
+                50 => 1,
+                52 => 1,
+                54 => 1,
+                56 => 1,
+                58 => 1,
+                60 => 1,
+                62 => 1,
+                64 => 1,
+                66 => 1,
+            ];
+        case 'FunctionTriggerErrorUnitTest.2.inc':
+        case 'FunctionTriggerErrorUnitTest.3.inc':
+        default:
+            return [];
+        }//end switch
 
     }//end getWarningList()
 
