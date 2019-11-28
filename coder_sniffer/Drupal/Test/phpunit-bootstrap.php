7f8ec4eb0fa91063ec4978a49689e0ca663363b3
Klaus Purer <klaus.purer@protonmail.ch>
2019-11-28 23:17:02 +0100

    fix indentation

--- coder_sniffer/Drupal/Test/phpunit-bootstrap.php
+++ coder_sniffer/Drupal/Test/phpunit-bootstrap.php
@@ -6,5 +6,5 @@ require 'vendor/squizlabs/php_codesniffer/autoload.php';
 
 // Support older versions of PHPUnit on older PHP versions.
 if (class_exists('PHPUnit_Framework_TestCase') === true && class_exists('PHPUnit\Framework\TestCase') === false) {
-  class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
