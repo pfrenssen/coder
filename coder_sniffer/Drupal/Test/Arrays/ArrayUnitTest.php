4b46fc1cd65c497b518ec48ac411a8cc336a07e4
Mitch Portier <mitch.portier@gmail.com>
2019-12-06 12:32:28 +0100

    Add configurable line limit to Drupal.Arrays.Array

--- coder_sniffer/Drupal/Test/Arrays/ArrayUnitTest.php
+++ coder_sniffer/Drupal/Test/Arrays/ArrayUnitTest.php
@@ -14,17 +14,27 @@ class ArrayUnitTest extends CoderSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of errors that should occur on that line.
      *
+     * @param string $testFile The name of the file being tested.
+     *
      * @return array(int => int)
      */
-    public function getErrorList()
+    public function getErrorList($testFile=null)
     {
-        return [
-            13 => 1,
-            33 => 1,
-            83 => 1,
-            88 => 1,
-            92 => 1,
-        ];
+        switch ($testFile) {
+        case 'ArrayUnitTest.1.inc':
+            return [
+                13 => 1,
+                33 => 1,
+                83 => 1,
+                88 => 1,
+                92 => 1,
+            ];
+        case 'ArrayUnitTest.2.inc':
+            return [
+                14 => 1,
+                17 => 1,
+            ];
+        }
 
     }//end getErrorList()
 
@@ -35,23 +45,47 @@ class ArrayUnitTest extends CoderSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of warnings that should occur on that line.
      *
+     * @param string $testFile The name of the file being tested.
+     *
      * @return array(int => int)
      */
-    public function getWarningList()
+    public function getWarningList($testFile=null)
     {
-        return [
-            17 => 1,
-            22 => 1,
-            23 => 1,
-            24 => 1,
-            37 => 1,
-            42 => 1,
-            44 => 1,
-            59 => 1,
-            76 => 1,
-        ];
+        switch ($testFile) {
+        case 'ArrayUnitTest.1.inc':
+            return [
+                17 => 1,
+                22 => 1,
+                23 => 1,
+                24 => 1,
+                37 => 1,
+                42 => 1,
+                44 => 1,
+                59 => 1,
+                76 => 1,
+            ];
+        case 'ArrayUnitTest.2.inc':
+            return [];
+        }
 
     }//end getWarningList()
+
+
+    /**
+     * Returns a list of test files that should be checked.
+     *
+     * @param string $testFileBase The base path that the unit tests files will have.
+     *
+     * @return array The list of test files.
+     */
+    protected function getTestFiles($testFileBase)
+    {
+        return [
+            __DIR__.'/ArrayUnitTest.1.inc',
+            __DIR__.'/ArrayUnitTest.2.inc',
+        ];
+
+    }//end getTestFiles()
 
 
 }//end class
