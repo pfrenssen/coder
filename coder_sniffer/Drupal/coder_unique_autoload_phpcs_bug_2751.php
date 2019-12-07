f139c17bbec40cfcfcc10174dbb2b483a62cfa8b
Klaus Purer <klaus.purer@protonmail.ch>
2019-12-07 16:55:40 +0100

    fix(autoload): Move autoloading to more unique file name to workaround PHPCS relative path confusion

--- coder_sniffer/Drupal/coder_unique_autoload_phpcs_bug_2751.php
+++ coder_sniffer/Drupal/coder_unique_autoload_phpcs_bug_2751.php
@@ -3,6 +3,8 @@
 /**
  * Includes classes which are not detected by PHP_CodeSniffer's autoloader.
  *
+ * This file has a weird name on purpose because of https://github.com/squizlabs/PHP_CodeSniffer/pull/2751
+ *
  * @category PHP
  * @package  PHP_CodeSniffer
  * @link     http://pear.php.net/package/PHP_CodeSniffer
