e7245605618294fee000207a84730410a891abc3
Arkener <mitch.portier@gmail.com>
2020-01-29 22:12:05 +0100

    Revert removal of PHPUnit for older versions

--- coder_sniffer/Drupal/Test/phpunit-bootstrap.php
+++ coder_sniffer/Drupal/Test/phpunit-bootstrap.php
@@ -3,3 +3,8 @@
 require 'vendor/autoload.php';
 require 'CoderSniffUnitTest.php';
 require 'vendor/squizlabs/php_codesniffer/autoload.php';
+
+// Support older versions of PHPUnit on older PHP versions.
+if (class_exists('PHPUnit_Framework_TestCase') === true && class_exists('PHPUnit\Framework\TestCase') === false) {
+    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
