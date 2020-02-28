ba2b4fafc539c046b146f0be1751d1df64c12c6c
Klaus Purer <klaus.purer@protonmail.ch>
2020-02-28 15:54:36 +0100

    tests(phpstan): Fix autoloading of constants and useless if() clause

--- coder_sniffer/Drupal/Test/phpunit-bootstrap.php
+++ coder_sniffer/Drupal/Test/phpunit-bootstrap.php
@@ -3,3 +3,5 @@
 require 'vendor/autoload.php';
 require 'CoderSniffUnitTest.php';
 require 'vendor/squizlabs/php_codesniffer/autoload.php';
+// PHPStan does not find the constants in this file, so load it manually.
+require_once 'vendor/squizlabs/php_codesniffer/src/Util/Tokens.php';
