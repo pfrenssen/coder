a36b23cc7984d6abac8d366f4e889eb87bd0d601
Arkener <mitch.portier@gmail.com>
2020-04-06 18:46:18 +0200

    feat(ExceptionT): Add sniff to ensure that exceptions are not translated

---
+++ coder_sniffer/DrupalPractice/Sniffs/General/ExceptionTSniff.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * \DrupalPractice\Sniffs\General\ExceptionTSniff.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+namespace DrupalPractice\Sniffs\General;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+/**
+ * Checks that exceptions aren't translated.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+class ExceptionTSniff implements Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array<int|string>
+     */
+    public function register()
+    {
+        return [T_THROW];
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+     * @param int                         $stackPtr  The position of the function
+     *                                               name in the stack.
+     *
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+
+        $tokens = $phpcsFile->getTokens();
+        $endPtr = $phpcsFile->findEndOfStatement($stackPtr);
+
+        for ($i = ($stackPtr + 1); $i < $endPtr; $i++) {
+            if ($tokens[$i]['code'] === T_STRING && $tokens[$i]['content'] === 't') {
+                $warning = 'Exceptions should not be translated';
+                $phpcsFile->addWarning($warning, $stackPtr, 'ExceptionT');
+                return;
+            }
+        }
+
+    }//end process()
+
+
+}//end class
