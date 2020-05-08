a7c6a3869e50a6e41ec2fa2dede5e3245a523635
Mitch Portier <mitch.portier@gmail.com>
2020-05-08 11:42:01 +0200

    feat(ExceptionT): Add sniff to ensure that exceptions are not translated (#3123061 by Arkener)

---
+++ coder_sniffer/DrupalPractice/Test/General/ExceptionTUnitTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace DrupalPractice\Test\General;
+
+use Drupal\Test\CoderSniffUnitTest;
+
+class ExceptionTUnitTest extends CoderSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getErrorList(string $testFile): array
+    {
+        return [];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getWarningList(string $testFile): array
+    {
+        return [
+            6  => 1,
+            10 => 1,
+        ];
+
+    }//end getWarningList()
+
+
+}//end class
