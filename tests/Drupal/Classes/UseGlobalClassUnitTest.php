2b6c0439fa17cbd221207dd60c744b3cb44b2024
Christoph Burschka <christoph@burschka.de>
2020-06-03 20:55:51 +0200

    Add a unit test for the UseGlobalClass sniff.

---
+++ tests/Drupal/Classes/UseGlobalClassUnitTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\Test\Classes;
+
+use Drupal\Test\CoderSniffUnitTest;
+
+class UseGlobalClassUnitTest extends CoderSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getErrorList(string $testFile): array
+    {
+        return [
+            8  => 1,
+            11 => 1,
+            12 => 1,
+            13 => 1,
+            15 => 1,
+        ];
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getWarningList(string $testFile): array
+    {
+        return [];
+
+    }//end getWarningList()
+
+
+    /**
+     * Returns a list of test files that should be checked.
+     *
+     * @param string $testFileBase The base path that the unit tests files will have.
+     *
+     * @return array<string>
+     */
+    protected function getTestFiles($testFileBase): array
+    {
+        return [
+            __DIR__.'/UseGlobalClassUnitTest.inc',
+        ];
+
+    }//end getTestFiles()
+
+
+}//end class
