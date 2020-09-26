14c4a4087b229ee2bc09fc325ab5eb7e2545bd13
Adam Zimmermann <adam@adamzimmermann.com>
2020-09-26 10:17:54 -0500

    Add unit test.

---
+++ tests/Drupal/Commenting/TodoCommentUnitTest.inc.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Test function.
+ *
+ * These are valid examples.
+ * @todo Valid.
+ * @todo Valid
+ * @todo This is valid.
+ * @todo this is valid.
+ *
+ * These should all be errors.
+ * @TODO Error
+ * @ToDo Error
+ * @TODo Error
+ * @ToDO Error
+ * @todo: Error
+ * @to-do Error
+ * @TO-DO Error
+ * @To-Do Error
+ * @TO do Error
+ * @to    do Error
+ * @todo: Error
+ * @todo : Error
+ * @todo- Error
+ * @todo - Error
+ * @todoError
+ * todo Error
+ * TODO Error
+ * ToDo Error
+ * @todo   Error
+ */
+function foo() {
+  // These are valid examples.
+  // @todo Valid.
+  // @todo Valid
+  // @todo This is valid.
+  // @todo this is valid.
+
+  // These should all be errors.
+  // @TODO Error
+  // @ToDo Error
+  // @TODo Error
+  // @ToDO Error
+  // @todo: Error
+  // @to-do Error
+  // @TO-DO Error
+  // @To-Do Error
+  // @TO do Error
+  // @to    do Error
+  // @todo: Error
+  // @todo : Error
+  // @todo- Error
+  // @todo - Error
+  // @todoError
+  // todo Error
+  // TODO Error
+  // ToDo Error
+  // @todo   Error
+}
