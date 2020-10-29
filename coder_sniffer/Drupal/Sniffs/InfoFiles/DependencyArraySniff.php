63d3820e7851a4a77a8b8b48854a28ceb842d1b6
Biswaranjan Pati <biswaranjan.pati@srijan.net>
2020-10-29 12:03:53 +0530

    Ensure module dependencies are an array. D.O #issue_id 3166470

---
+++ coder_sniffer/Drupal/Sniffs/InfoFiles/DependencyArraySniff.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * \Drupal\Sniffs\InfoFiles\DependencyArraySniff.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+namespace Drupal\Sniffs\InfoFiles;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * "dependency" should have single entry as an array in Drupal 8 info files.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+class DependencyArraySniff implements Sniff {
+
+
+  /**
+   * Returns an array of tokens this test wants to listen for.
+   *
+   * @return array<int|string>
+   */
+  public function register() {
+    return [T_INLINE_HTML];
+
+  }//end register()
+
+
+  /**
+   * Processes this test, when one of its tokens is encountered.
+   *
+   * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+   * @param int $stackPtr The position of the current token in the
+   *                                               stack passed in $tokens.
+   *
+   * @return int
+   */
+  public function process(File $phpcsFile, $stackPtr) {
+    // Only run this sniff once per info file.
+    $fileExtension = strtolower(substr($phpcsFile->getFilename(), -8, 4));
+    if ($fileExtension !== 'info') {
+      return ($phpcsFile->numTokens + 1);
+    }
+
+    $info = Yaml::parse(file_get_contents($phpcsFile->getFilename()));
+    if (isset($info['dependencies']) === TRUE) {
+      if (!is_array($info['dependencies'])) {
+        $error = '"dependencies" should be an array in the info yaml file';
+        $phpcsFile->addError($error, $stackPtr, 'Dependencies');
+      }
+    }
+
+    return ($phpcsFile->numTokens + 1);
+
+  }//end process()
+
+
+}//end class
