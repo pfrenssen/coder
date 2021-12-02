1e6c7b471ef35344d5b0f52dc5a3942c48f2bef3
Shawn McCabe <smccabe@acromediainc.com>
2021-12-02 14:39:08 -0800

    Add waitFor return check since waitForText doesn't assert

---
+++ tests/DrupalPractice/FunctionCalls/WaitForTextUnitTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Unit test class for the DbQuery sniff.
+ */
+
+namespace DrupalPractice\Test\FunctionCalls;
+
+use Drupal\Test\CoderSniffUnitTest;
+
+/**
+ * Unit test class for the WaitForText sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ */
+class WaitForTextUnitTest extends CoderSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getErrorList(string $testFile): array
+    {
+        return [];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getWarningList(string $testFile): array
+    {
+        return [
+            11 => 1,
+            14 => 1,
+        ];
+
+    }//end getWarningList()
+
+
+}//end class
