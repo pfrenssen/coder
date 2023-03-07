deaec730f31e8529c67bc9d247612c094a33616d
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2023-03-07 15:06:30 +0000

    feat(DependenciesArray): Ensure module dependencies are an array in .info.yml (#3166470)

---
+++ tests/Drupal/InfoFiles/DependenciesArrayUnitTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\Test\InfoFiles;
+
+use Drupal\Test\CoderSniffUnitTest;
+
+/**
+ * Unit test class for the DependenciesArray sniff.
+ */
+class DependenciesArrayUnitTest extends CoderSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getErrorList(string $testFile): array
+    {
+        return [3 => 1];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getWarningList(string $testFile): array
+    {
+        return [];
+
+    }//end getWarningList()
+
+
+    /**
+     * Returns a list of test files that should be checked.
+     *
+     * @param string $testFileBase The base path that the unit tests files will have.
+     *
+     * @return array<string>
+     */
+    protected function getTestFiles($testFileBase): array
+    {
+        return [
+            __DIR__.'/drupal8/dependencies_array.info.yml',
+        ];
+
+    }//end getTestFiles()
+
+
+}//end class
