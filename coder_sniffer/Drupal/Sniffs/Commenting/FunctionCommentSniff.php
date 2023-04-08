8aaf5745326ce47014131e8fa0181a01837bd009
Klaus Purer <klaus.purer@protonmail.ch>
2023-04-08 13:07:41 +0200

    better comment

--- coder_sniffer/Drupal/Sniffs/Commenting/FunctionCommentSniff.php
+++ coder_sniffer/Drupal/Sniffs/Commenting/FunctionCommentSniff.php
@@ -1014,7 +1014,7 @@ class FunctionCommentSniff implements Sniff
             return $type;
         }
 
-        // Also allow "-" for special type hint "array-key" supported by PHPStan
+        // Also allow "-" and "<>" for special type hints supported by PHPStan
         // https://phpstan.org/writing-php-code/phpdoc-types#basic-types .
         $type = preg_replace('/[^a-zA-Z0-9_\\\[\]\-<>]/', '', $type);
 
