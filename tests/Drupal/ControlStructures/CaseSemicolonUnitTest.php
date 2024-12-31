ce445b9f5e90843d882cddfb161e5b6e6bcec8a6
Klaus Purer <klaus.purer@protonmail.ch>
2024-12-31 14:40:28 +0100

    feat(CaseSemicolon): Add sniff to check semicolons on case statements (#3487111)

---
+++ tests/Drupal/ControlStructures/CaseSemicolonUnitTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\Test\ControlStructures;
+
+use Drupal\Test\CoderSniffUnitTest;
+
+class CaseSemicolonUnitTest extends CoderSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getErrorList(string $testFile): array
+    {
+        return [
+            13 => 1,
+            17 => 1,
+        ];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array<int, int>
+     */
+    protected function getWarningList(string $testFile): array
+    {
+        return [];
+
+    }//end getWarningList()
+
+
+}//end class
