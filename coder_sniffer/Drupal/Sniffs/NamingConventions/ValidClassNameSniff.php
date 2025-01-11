7ea70d08aa59febd4019a66b90d3daa1f2fe7823
Klaus Purer <klaus.purer@protonmail.ch>
2025-01-11 16:52:19 +0100

    fix(ValidClassName): Allow upper case with number

--- coder_sniffer/Drupal/Sniffs/NamingConventions/ValidClassNameSniff.php
+++ coder_sniffer/Drupal/Sniffs/NamingConventions/ValidClassNameSniff.php
@@ -76,7 +76,7 @@ class ValidClassNameSniff implements Sniff
         // @todo We could make this more strict to check if there are more than
         // 2 upper case characters in a row, but not decided yet.
         // See https://www.drupal.org/project/coder/issues/3497433
-        if (strtoupper($name) === $name) {
+        if (preg_match('|^[A-Z]{3}[A-Z_]*$|', $name) === 1) {
             $error = '%s name must use UpperCamel naming and not contain multiple upper case letters in a row';
             $phpcsFile->addError($error, $stackPtr, 'NoUpperAcronyms', $errorData);
         }
