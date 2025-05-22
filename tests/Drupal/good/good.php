8e93fa1dc61f604a63bcde1a5474979a06a6d613
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2025-05-22 16:03:54 +0100

    revert unintended trailing-blank change

--- tests/Drupal/good/good.php
+++ tests/Drupal/good/good.php
@@ -1124,7 +1124,7 @@ class Foo implements FooInterface {
 
 }
 
-t('Some long mulit-line
+t('Some long mulit-line 
   text is weird, but allowed.');
 
 // Anonymous functions should not throw indentation errors here.
