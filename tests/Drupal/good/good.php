0e38b85c390b11af8eb577a0f89ce5d821bc801c
Klaus Purer <klaus.purer@protonmail.ch>
2025-10-18 15:18:31 +0200

    fix good tests

--- tests/Drupal/good/good.php
+++ tests/Drupal/good/good.php
@@ -1397,17 +1397,6 @@ class OperatorTest {
 
 }
 
-// Namespaced function call is allowed because PHP 5.5 and lower do not support
-// use statements for functions.
-$default_config = [
-  'verify' => TRUE,
-  'timeout' => 30,
-  'headers' => [
-    'User-Agent' => 'Drupal/' . \Drupal::VERSION . ' (+https://www.drupal.org/) ' . \GuzzleHttp\default_user_agent(),
-  ],
-  'handler' => $stack,
-];
-
 // camelCase and snake_case variables are allowed.
 $snake_case = 1;
 $camelCase = 1;
